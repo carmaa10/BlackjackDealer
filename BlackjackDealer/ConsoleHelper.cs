c7d35082ca869a5ac76897976f5af15fc3aaf163
Carma Aten <carmaea10@gmail.com>
2019-11-18 12:53:36 -0500

    progress 11/18/19

---
+++ BlackjackDealer/ConsoleHelper.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// ************************************************************
+// Class taken from user Manfred Radlwimmer on Stack Overflow
+// Adapted to fit my needs
+// https://stackoverflow.com/a/46909420
+// Used to make a multiple choice highlighted menu with enter 
+// to select
+// ************************************************************
+
+namespace BlackjackDealer
+{
+    class ConsoleHelper
+    {
+        public static int MultipleChoice(bool canCancel, params string[] options)
+        {
+            const int startX = 8;
+            const int startY = 4;
+            const int optionsPerLine = 1;
+            const int spacingPerLine = 14;
+
+            int currentSelection = 0;
+
+            ConsoleKey key;
+
+            Console.CursorVisible = false;
+
+            do
+            {
+                Console.Clear();
+
+                for (int i = 0; i < options.Length; i++)
+                {
+                    Console.SetCursorPosition(startX + (i % optionsPerLine) * spacingPerLine, startY + i / optionsPerLine);
+
+                    if (i == currentSelection)
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.Write(options[i]);
+
+                    Console.ResetColor();
+                }
+
+                key = Console.ReadKey(true).Key;
+
+                switch (key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        {
+                            if (currentSelection % optionsPerLine > 0)
+                                currentSelection--;
+                            break;
+                        }
+                    case ConsoleKey.RightArrow:
+                        {
+                            if (currentSelection % optionsPerLine < optionsPerLine - 1)
+                                currentSelection++;
+                            break;
+                        }
+                    case ConsoleKey.UpArrow:
+                        {
+                            if (currentSelection >= optionsPerLine)
+                                currentSelection -= optionsPerLine;
+                            break;
+                        }
+                    case ConsoleKey.DownArrow:
+                        {
+                            if (currentSelection + optionsPerLine < options.Length)
+                                currentSelection += optionsPerLine;
+                            break;
+                        }
+                    case ConsoleKey.Escape:
+                        {
+                            if (canCancel)
+                                return -1;
+                            break;
+                        }
+                }
+            } while (key != ConsoleKey.Enter);
+
+            Console.CursorVisible = true;
+
+            return currentSelection;
+        }
+    }
+}
---
+++ BlackjackDealer/Program.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+// NOTES TO ME TO DELETE WHEN TURN IN
+// If any of my search terms are still in the program take care of them then delete this
+// * RID = get rid of later, for easy find later
+// * MAKE BETTER = flesh out/make pretty, for easy find later
+// * IMPLEMENT = need to add a thing, for easy find later
+// * HERE = where I left off, for easy find later
+
+namespace BlackjackDealer
+{
+    class Program
+    {
+        // ************************************
+        // Title: Blackjack Dealter
+        // Application Type: Console .NET Core
+        // Description: MAKE BETTER
+        // Author: Carma Aten
+        // Date Created: 11/17/19
+        // Last Modified: 11/18/19
+        // ************************************
+
+        static void Main(string[] args)
+        {
+
+            DisplayWelcomeScreen("Blackjack Dealer");
+            DisplayMainMenu();
+            DisplayClosingScreen("Thank you for playing my unoriginal avaerage game!");
+
+        }
+
+        #region SCREENS
+
+        static void DisplayWelcomeScreen(string message)
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("\t\t" + message);
+            Console.WriteLine();
+
+            DisplayContinuePrompt("start");
+        }
+
+        static void DisplayMainMenu()
+        {
+            // -------------
+            // VARIABLES
+            //
+            ConsoleColor foregroundColor;
+            ConsoleColor backgroundColor;
+
+            string cardDeckInfo = @"Data/cardDeckInfo.txt";
+            string instructions = @"Data/instructions.txt";
+
+            // string userResponse;
+            int userResponse;
+            bool runApplication = true;
+
+
+
+            while (runApplication)
+            {
+
+                DisplayScreenHeader("Main Menu");
+
+                userResponse = ConsoleHelper.MultipleChoice(true, "Play Game", "Display Instructions", "Rules", "Players");
+                userResponse += 1; // Imporoving program readability so that the first options is option #1
+                switch (userResponse)
+                {
+                    case 1:
+                        DisplayErrorMessage("Under Development RID");
+                        break;
+                    case 2:
+                        DisplayLongGameInstructions(instructions);
+                        break;
+                    case 3:
+                        DisplayErrorMessage("Under Development RID");
+                        break;
+                    case 4:
+                        DisplayErrorMessage("Under Development RID");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        #region MAIN MENU SCREENS
+
+        /// <summary>
+        /// Takes all the text from the instructions.txt file and prints it out on the screen line by line
+        /// </summary>
+        /// <param name="instructions">The file path for the instructions.txt file</param>
+        // IDEAS
+        // * Have menu for everything other than object of game -- use an array in an array??
+        static void DisplayLongGameInstructions(string instructions)
+        {
+            Console.Clear();
+
+            string[] instructionsText = File.ReadAllLines(instructions);
+            foreach (string line in instructionsText)
+            {
+                Console.WriteLine("\t" + line);
+            }
+
+            DisplayContinuePrompt("return to the main menu");
+        }
+
+        #endregion // MAIN MENU SCREENS
+
+        static void DisplayClosingScreen(string message)
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("\t\t" + message);
+            Console.WriteLine();
+
+            DisplayContinuePrompt("exit");
+        }
+
+        #endregion // SCREENS
+
+        // MAKE BETTER need better name for region
+        #region CARD STUFF
+
+        static (string suit, string rank, int value)[] BuildCardDeck(string cardDeckInfo)
+        {
+            (string suit, string rank, int value) cardInfo;
+
+            // HERE is where I left off
+
+            return cardInfo;
+        }
+
+        #endregion
+
+        #region HELPER METHODS
+
+        static void DisplayContinuePrompt(string action)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Press enter to {action}.");
+            Console.ReadLine();
+        }
+
+        static void DisplayScreenHeader(string headerText)
+        {
+            int dashRepeat = 74; // a constant throughout the program, when I have a line for a main heading, it's 74 characters long
+
+            Console.Clear();
+            Console.WriteLine();
+            RepeatCharacter(dashRepeat, "-");
+            Console.WriteLine("\n\t\t" + headerText);
+            RepeatCharacter(dashRepeat, "-");
+            Console.WriteLine();
+        }
+
+        static string DisplayGetUserResponse(string question)
+        {
+            string userResponse;
+
+            Console.WriteLine(question);
+
+            userResponse = Console.ReadLine().Trim();
+
+            return userResponse;
+        }
+
+        static bool CheckParseWorked(bool parse)
+        {
+            if (parse)
+            {
+                parse = true;
+            }
+            else
+            {
+                DisplayErrorMessage("Please input a number (10, 2, etc.)");
+                parse = false;
+            }
+            return parse;
+        }
+
+        static void DisplayErrorMessage(string error)
+        {
+            Console.WriteLine($"\n** {error} **");
+
+            DisplayContinuePrompt("retry");
+        }
+
+        static void RepeatCharacter(int numberTimes, string character)
+        {
+            for (int repeat = 0; repeat < numberTimes; repeat++)
+            {
+                Console.Write(character);
+            }
+        }
+
+        #endregion // HELPER METHODS
+    }
+}
